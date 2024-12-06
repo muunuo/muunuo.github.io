ea6bf3be42475e2d3efe0dcc16c19d320f64fac5
muunuo <benkforland@gmail.com>
2024-12-06 08:51:43 +0100

    fikset bilder/kommentarer
    skriv inn komentarer og byttet navn på teknologiforsåelse mappe grunnet at å først til at bilder ikke vises

--- Javascript/JsJavascriptside/inputfelt.js
+++ Javascript/JsJavascriptside/inputfelt.js
@@ -1,20 +1,21 @@
 let brukernavn = document.getElementById("brukerenSittNavn"); // gjør brukernavnet om til variabel
-let knapp = document.getElementById("knapp");
-let siHei = document.getElementById("hei");
-let navn = document.getElementById("sierBrukerNavn");
+let trykkKnapp = document.getElementById("knapp"); // husk forskjellige navn på id og variabel
+let siHei = document.getElementById("hei"); //(husk at let er det nye navnet, det i "er det gammle".)
+let navn = document.getElementById("sierBrukerNavn"); 
 
-siHei.style.display = "none";
+siHei.style.display = "none"; //ordet hei er gjømt når programet åpnes
 
-knapp.addEventListener("click", sendInnBrukernavn); //"click" er innebyggd
-//click sender inn brukernavn
+trykkKnapp.addEventListener("click", sendInnBrukernavn); //"click" er innebyggd, 
+//gjør at når knapp trykkes blir navnet gitt variabelen sendInnBrukernavn
 function sendInnBrukernavn (){ //når send in brukernavn blir aktivert skal dette skje
-    let brukernavn = brukerenSittNavn.value;
+    let brukernavn = brukerenSittNavn.value; //
 
-    console.log(brukernavn);
-    knapp.style.display = "none";
-    brukerenSittNavn.style.display = "none";
-    siHei.style.display = "block";
+    console.log(brukernavn); //sier brukernavn i konsollen
 
-    document.getElementById("sierBrukerNavn").innerHTML = brukernavn;
+    trykkKnapp.style.display = "none"; //gjømmer knappen
+    brukerenSittNavn.style.display = "none"; //gjømmer svarfeltet
+    siHei.style.display = "block"; //viser hei
+
+    document.getElementById("sierBrukerNavn").innerHTML = brukernavn; //Brukernavnet skrives på nettsiden
 }
 
--- Javascript/JsJavascriptside/profil.js
+++ Javascript/JsJavascriptside/profil.js
@@ -1,9 +1,20 @@
+
 let bilde = document.querySelector("profilbilde");
 
-bilde.addEventListener("click", visBildeValg());
+bilde.addEventListener("click", visBildeValg);
 
 function visBildeValg() {
     console.log ("hello")
 
 
 }
+// sett opp og gjør bilde 1 til en knapp
+
+// sett opp og gjør bilde 2 til en knapp
+
+// Gjøm bilde 2
+
+// Når bilde 1 trykkes gjøm det og vis bilde 2
+
+//gjør samme for når bilde to trykkes
+
