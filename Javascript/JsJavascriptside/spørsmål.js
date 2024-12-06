b8f75ca4dcaba6abe46f507d8191d6f522f2a9e3
muunuo <benkforland@gmail.com>
2024-12-06 10:58:42 +0100

    Sender inn svar fra mer en en input
    Fikk til å sende inn svar fra mer en et input felt no. Fant ut at jeg ikke kunne ha input felte inni p (inni settningen) hvis det skulle fungere (gir mening men ja) Lagte ny to-do grunnet at jeg slettet den andre. Skal no jobbe på å få inn riktige svar alternativ om alt det

--- Javascript/JsJavascriptside/spørsmål.js
+++ Javascript/JsJavascriptside/spørsmål.js
@@ -1,13 +1,15 @@
 
 
 let svarSpørsmål1 = document.getElementById("brukerSittSvarSpørsmål1"); // gjør brukernavnet om til variabel
-let trykkKnapp = document.getElementById("sendInnAlleSvar"); // husk forskjellige navn på id og variabel
+let sendInn = document.getElementById("sendInnAlleSvar"); // husk forskjellige navn på id og variabel
+let svarSpørsmål2 = document.getElementById("brukerSittSvarSpørsmål2");
 
 
-trykkKnapp.addEventListener("click", sendInnSvar); //"click" er innebyggd, 
+sendInn.addEventListener("click", sendInnSvar); //"click" er innebyggd, 
 //sier at når knappen trykkes skal den gjøre sendInnBrukernavn under.
 function sendInnSvar (){ //når send in brukernavn blir aktivert skal dette skje
     let svarSpørsmål1 = brukerSittSvarSpørsmål1.value; //lagrer brukernavnet som ble skrvet inn i feltet
-
+    let svarSpørsmål2 = brukerSittSvarSpørsmål2.value;
     console.log(svarSpørsmål1); //sier brukernavn i konsollen
+    console.log(svarSpørsmål2)
 }
