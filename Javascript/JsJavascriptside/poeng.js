d1260bbe664dae8887e6cca812cfc6661c43b6e2
muunuo <benkforland@gmail.com>
2024-12-06 13:26:11 +0100

    jobbet med quiz
    jobbet videre på det med å få spørsmålskjemaet til å fungere slik det skal. Tror jeg må gjøre det ganske om for at det skalfungere

---
+++ Javascript/JsJavascriptside/poeng.js
@@ -0,0 +1,8 @@
+
+let poeng = 0 
+
+
+if (svar == svarx[x]) {
+    let sum= poeng + 1
+    console.log("sum")
+}
--- Javascript/JsJavascriptside/spørsmål.js
+++ Javascript/JsJavascriptside/spørsmål.js
@@ -4,12 +4,41 @@ let svarSpørsmål1 = document.getElementById("brukerSittSvarSpørsmål1"); // g
 let sendInn = document.getElementById("sendInnAlleSvar"); // husk forskjellige navn på id og variabel
 let svarSpørsmål2 = document.getElementById("brukerSittSvarSpørsmål2");
 
+//la data vite hva rett svar er 
 
-sendInn.addEventListener("click", sendInnSvar); //"click" er innebyggd, 
-//sier at når knappen trykkes skal den gjøre sendInnBrukernavn under.
+let spørsmålx= document.querySelector("spørsmål")
+
+svarx =["CSS","px"]
+
+//lage svar knapp
+sendInn.addEventListener("click", sendInnSvar); //"click" er innebyggd,
+
+//få svar knappen til å sende inn svar for begge spørsmålene
 function sendInnSvar (){ //når send in brukernavn blir aktivert skal dette skje
     let svarSpørsmål1 = brukerSittSvarSpørsmål1.value; //lagrer brukernavnet som ble skrvet inn i feltet
     let svarSpørsmål2 = brukerSittSvarSpørsmål2.value;
     console.log(svarSpørsmål1); //sier brukernavn i konsollen
     console.log(svarSpørsmål2)
-}
+
+//få mulighet til å svarknappen til å forsvinne etter man trykker svar
+    sendInn.style.display = "none"
+
+    //gjømme boksene man skriver i å skrive svaret bruker ga med <p>
+
+    //la bruker vite hva rett svar er 
+    for (let x = 0; x < spørsmålx.length; x++) {
+        let svar = console.log (spørsmålx)[x];
+        if (svar == svarx[x]) {
+            console.log("rett")
+        } else {
+            console.log("feil")
+        }
+            
+        }
+        
+    }
+
+//data sier om bruker svarte rett eller feil
+
+//Ha en poengsum
+
