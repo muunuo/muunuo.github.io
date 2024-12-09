f3fab3d947f8faf495316c7b1f5074801d110717
muunuo <benkforland@gmail.com>
2024-12-09 14:26:37 +0100

    dressUpGame
    Gjorde ferdig poengsystem, jobbet på. åsette opp knapper til å kunne velge farger. Begynte på et dressuop Game

---
+++ Javascript/JsJavascriptside/endreFarge.js
@@ -0,0 +1,14 @@
+// let tekstfarge = prompt ("What color should the text be?")
+// document.querySelector("#tekstfarge").style.color = tekstfarge
+
+
+//Sette opp 4 farger man kan velge mellom
+
+//gjør fargene til en knapp
+//Når knapp trykkes endres fargen
+
+//Ekstra:
+// Gjør at det er en kombo text/farge endring basert på hva som er mer synelig, bruk et bilde av text på den fargen for å vise hvordan det vil se ut.
+//Når du hovrer vises det, npår du trykkes aktiveres det
+
+
