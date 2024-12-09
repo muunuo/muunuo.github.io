d30652eaf3c27d35357f121a698001d0fd6e4fc4
muunuo <benkforland@gmail.com>
2024-12-09 10:37:27 +0100

    poeng
    Jobbet mer på poeng og et par navn/kommentarer

--- Javascript/JsJavascriptside/poeng.js
+++ Javascript/JsJavascriptside/poeng.js
@@ -1,10 +1,16 @@
 
-let poeng = 0 
+let poengsum = 0 
+
 
 if (svarSpørsmål1==svar1) {
-    poeng+1
+    poengsum=poengsum+1
 }
 
 if (svarSpørsmål2==svar2) {
-    poeng+1
+    poengsum=poengsum+1
 }
+
+document.getElementById("poengsum").innerHTML = poengsum; 
+//sette opp så den teller poeng per spørsmål
+//sette opp så den vet mengden spørsmål(?)
+//sette opp så den kan si mengden riktige svar i forhold til mengden spørsmål 
--- Javascript/JsJavascriptside/spørsmål.js
+++ Javascript/JsJavascriptside/spørsmål.js
@@ -2,16 +2,16 @@
 let svarSpørsmål1 = document.getElementById("brukerSittSvarSpørsmål1"); // gjør brukernavnet om til variabel
 let sendInn = document.getElementById("sendInnAlleSvar"); // husk forskjellige navn på id og variabel
 let svarSpørsmål2 = document.getElementById("brukerSittSvarSpørsmål2");
-let rett1 = document.getElementById("rettSvar1")
-let rett2 =document.getElementById("rettSvar2")
+let rett1 = document.getElementById("SierRettSvar1"); //hvis bruker svarer feil blir rett svar gitt
+let rett2 =document.getElementById("SierRettSvar2"); //hvis bruker svarer feil blir rett svar gitt
 
 //vise hva rette svaret er
-rettSvar1.style.display="none"
-rettSvar2.style.display="none"
+SierRettSvar1.style.display="none"; //gjømmer rett svar frem til de trengs
+SierRettSvar2.style.display="none"; //gjømmer rett svar frem til de trengs
 
 //la data vite hva rett svar er 
-svar1 = "CSS"
-svar2 ="px"
+svar1 = "CSS";
+svar2 ="px";
 
 //lage svar knapp
 sendInn.addEventListener("click", sendInnSvar); //"click" er innebyggd,
@@ -21,35 +21,35 @@ function sendInnSvar (){ //når send in brukernavn blir aktivert skal dette skje
     let svarSpørsmål1 = brukerSittSvarSpørsmål1.value; //lagrer brukernavnet som ble skrvet inn i feltet
     let svarSpørsmål2 = brukerSittSvarSpørsmål2.value;
     console.log(svarSpørsmål1); //sier brukernavn i konsollen
-    console.log(svarSpørsmål2)
+    console.log(svarSpørsmål2);
 
 //få svarknappen til å forsvinne etter man trykker svar
-    sendInn.style.display="none"
+    sendInn.style.display="none";
 
     //svaret kan ikke redigeres 
     document.getElementById("brukerSittSvarSpørsmål1").disabled= true;
     document.getElementById("brukerSittSvarSpørsmål2").disabled= true;
 
-//hva skjer når du svarer
+
+//la bruker vite hva rett svar er 
     if (svarSpørsmål1==svar1) {
-        console.log("1rett")
+        console.log("1rett");
     } else {
-        console.log("1feil")
-        rettSvar1.style.display="block"
+        console.log("1feil");
+        SierRettSvar1.style.display="block";
     }
 
     if (svarSpørsmål2==svar2) {
-        console.log("2rett")
+        console.log("2rett");
     } else {
-        console.log("2feil")
-        rettSvar2.style.display="block"
+        console.log("2feil");
+        SierRettSvar2.style.display="block";
     }
 
 }
 
 // //setter opp så input kan fjernes og svar vises 
-//gjømme boksene man skriver i å skrive svaret bruker ga med <p>
-//la bruker vite hva rett svar er 
+
 //data sier om bruker svarte rett eller feil
 
 //Ha en poengsum
