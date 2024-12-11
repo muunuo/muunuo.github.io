614cb4b08444b80a6c62b1e80fdf768cb7ffe4a9
muunuo <benkforland@gmail.com>
2024-12-11 12:32:35 +0100

    posisjonering
    Posisjonering av ting, grønn kjole forsvant så kvittet meg med det. Begynte på js

---
+++ Javascript/JsJavascriptside/dressUpGame.js
@@ -0,0 +1,8 @@
+let bildeAntrekk1 = document.getElementById("rosaKjole");
+let bildeAntrekk2 = document.getElementById("antrekk");
+
+let brukerSittValg = "";
+
+bildeAntrekk1.addEventListener("click", velgAntrekk1);
+bildeAntrekk2.addEventListener("click", velgAntrekk2);
+
