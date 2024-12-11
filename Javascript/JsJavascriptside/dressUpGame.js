9862bd4f29466bfe405f6e0eb16b7d7fa7748848
muunuo <benkforland@gmail.com>
2024-12-11 15:46:24 +0100

    Masse
    Prøvde lenge å få alt til å gå. Men koden var for rotete med true og fals pså endte opp å begynne på ny og skrive deler av koden om. Sjole 1 kan no fjernes og vises med å trykke på den

--- Javascript/JsJavascriptside/dressUpGame.js
+++ Javascript/JsJavascriptside/dressUpGame.js
@@ -1,8 +1,116 @@
-let bildeAntrekk1 = document.getElementById("rosaKjole");
-let bildeAntrekk2 = document.getElementById("antrekk");
+//sett opp variabler
+let KnappVelgAntrekk1 = document.getElementById("litenRosaKjole");
+let KnappVelgAntrekk2 = document.getElementById("litenAntrekk");
 
-let brukerSittValg = "";
+let bildeAntrekkNaken = document.getElementById("nakenKlær")
+let antrekk1 = document.getElementById("rosaKjole");
+let antrekk2 = document.getElementById("antrekk");
+console.log(antrekk1)
+//gjøm antrekkene
+let antrekk1Vises =false //antrekket er av ved start
+let antrekk2Vises =false //antrekket er av ved start
 
-bildeAntrekk1.addEventListener("click", velgAntrekk1);
-bildeAntrekk2.addEventListener("click", velgAntrekk2);
 
+let nakenKlærVises =true //naken antrekket er på ved start
+
+
+
+//lag små antrekk om til knapper
+KnappVelgAntrekk1.addEventListener("click", velgAntrekk1);
+KnappVelgAntrekk2.addEventListener("click", velgAntrekk2);
+
+
+
+//Hvis antrek x velges gjømmes alle andre antrekk
+//true skrur på et antrekk og skrur av de andre antrekkene 
+function visesEllerIkke() {
+    if (antrekk1Vises==false) { //sier at når fals vises ikke antrekket
+        antrekk1.style.display="none"
+    }else{
+        antrekk1.style.display="block"
+        antrekk2Vises=false
+        nakenKlærVises=false
+    }
+    
+    if (antrekk2Vises==false) {
+        antrekk2.style.display="none"
+    }else{
+        antrekk2.style.display="block"
+        antrekk1Vises=false
+        nakenKlærVises=false
+    }
+    
+    if (nakenKlærVises==false) {
+        bildeAntrekkNaken.style.display="none"
+    }else{
+        bildeAntrekkNaken.style.display="block"
+        antrekk1Vises=false
+        antrekk2Vises=false
+    }
+    
+    // if (antrekk1Vises==true) { 
+    //     antrekk1.style.display="block"
+    //     antrekk2=false
+    //     bildeAntrekkNaken=false
+    // }
+}
+
+visesEllerIkke()
+
+
+function velgAntrekk1() {
+    
+    console.log("knapp1 trykkes")
+    
+    if (antrekk1Vises==false) { //hvis hun ikke har antrekket får hun det
+        antrekk1Vises=true
+    } else { //hvis hun har det fjernes det og nakenKlær vises
+        antrekk1Vises=false
+        nakenKlærVises=true //så når antrekket tas av, så er ikke hun naken
+    }
+
+    visesEllerIkke()
+    //løp funksjonen så ting faktisk skjer.
+
+//hvis bilde 2 er på når bilde 1 velges skal bilde 2 skrus av
+}
+
+
+function velgAntrekk2() {
+    
+    console.log("knapp1 trykkes")
+    
+    if (antrekk2Vises==false) { //hvis hun ikke har antrekket får hun det
+        antrekk2Vises=true
+    } else { //hvis hun har det fjernes det og nakenKlær vises
+        antrekk2Vises=false
+        nakenKlærVises=true //så når antrekket tas av, så er ikke hun naken
+    }
+    visesEllerIkke()
+
+    //løp funksjonen så ting faktisk skjer.
+
+//hvis bilde 2 er på når bilde 1 velges skal bilde 2 skrus av
+}
+
+
+
+
+
+
+
+
+//kan man gi en kjennerel ordre om at hvis x=fals skjer x.style.display="none", uansett hva som er fals?
+
+//Hvis antrekk x velges skal alt annet gjømmes og den vises
+// function velgAntrekk1() {
+//     console.log("knapp1 trykkes")
+//     // ikke testet
+// //     let bildeAntrekk1=true //da vet pcen at denne er på
+
+// // //hvis bilde 2 er på når bilde 1 velges skal bilde 2 skrus av
+// //     if (bildeAntrekk2==true) { //ber pc kjekke om antrekk to er på
+// //         bildeAntrekk2=false //hvis antrekk to er på skal det av
+// //         bildeAntrekk2.style.display="none" //antrekk to skal ikke være synelig når antrekk to er av
+// //     }
+// }
---
+++ Javascript/JsJavascriptside/dressupGame2.js
@@ -0,0 +1,71 @@
+let KnappVelgAntrekk1 = document.getElementById("litenRosaKjole");
+let KnappVelgAntrekk2 = document.getElementById("litenAntrekk");
+
+let bildeAntrekkNaken = document.getElementById("nakenKlær");
+let antrekk1 = document.getElementById("rosaKjole");
+let antrekk2 = document.getElementById("antrekk");
+
+
+//2. Det begynner som av
+let antrekk1TogF =false;
+let antrekk2TogF =false;
+
+
+
+//1. Lag en knapp
+KnappVelgAntrekk1.addEventListener("click", velgAntrekk1);
+KnappVelgAntrekk2.addEventListener("click", velgAntrekk2);
+
+
+function TogF() { //her inne skal vi si hva som skjer når noe er sant eller usant.
+    if (antrekk1TogF==false) { //om antrekket er av
+        antrekk1.style.display="none" //skal det ikke vises
+        } else { //men hvis det ikke er av (altså på)
+            antrekk1.style.display="block" //skal det vises
+        }
+
+        if (antrekk2TogF==false) { 
+            antrekk2.style.display="none"
+            } else {
+                antrekk2.style.display="block" 
+            }
+}
+
+TogF() //function TogF blir kjørt her 
+
+
+function velgAntrekk1() {
+console.log("Rosa ble trykket")
+    //løp funksjonen så ting faktisk skjer.
+    if (antrekk1TogF==false) {//3. Når trykket blir den til på
+        antrekk1TogF=true 
+    } else {//4.når trykket igjen blir den på
+        antrekk1TogF=false
+    }
+    TogF() //functionen blir kjørt
+}
+
+function velgAntrekk2() {
+    console.log("annet ble trykket")
+        //løp funksjonen så ting faktisk skjer.
+        if (antrekk2TogF==false) {//3. Når trykket blir den til på
+            antrekk2TogF=true 
+        } else {//4.når trykket igjen blir den på
+            antrekk2TogF=false
+        }
+        TogF() //functionen blir kjørt
+    }
+
+
+
+
+
+//1,2. gjør samme som over for nr2
+//1,3. hvis nr 1 er på, og nr2 trykkes, slås 1 av og 2 på.
+
+
+
+function velgAntrekk2() {
+
+
+}
