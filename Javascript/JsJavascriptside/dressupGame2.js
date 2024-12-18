fcbc44aa27ae3fabeb97d116cfbc9df25d755f09
muunuo <benkforland@gmail.com>
2024-12-18 10:32:50 +0100

    Fikset dressup
    Prøvde et par forskjellige metoder å få antrekk byttingen til å fungere (de hvite "naken" antrekke forsvant ikke, og de fungerte ikke å gå fra antrekk 1 til 2 direkte), til slutt fant jeg ut at den beste løsningen for no er bare å fjerne "naken" antrekke, og heller ha det være en del av "dokken" altid, som bare dekkes av klær

--- Javascript/JsJavascriptside/dressupGame2.js
+++ Javascript/JsJavascriptside/dressupGame2.js
@@ -1,26 +1,61 @@
 let KnappVelgAntrekk1 = document.getElementById("litenRosaKjole");
 let KnappVelgAntrekk2 = document.getElementById("litenAntrekk");
 
-let bildeAntrekkNaken = document.getElementById("nakenKler");
+let antrekk0 = document.getElementById("nakenKler");
 let antrekk1 = document.getElementById("rosaKjole");
 let antrekk2 = document.getElementById("antrekk");
 
+let neste= document.getElementById("nesteKnapp");
+let forige = document.getElementById("forigeKnapp");
 
 //2. Det begynner som av
-let antrekk1TogF =false;
-let antrekk2TogF =false;
-
+let antrekk1TogF=false;
+let antrekk2TogF=false;
 
 //1. Lag en knapp
 KnappVelgAntrekk1.addEventListener("click", velgAntrekk1);
 KnappVelgAntrekk2.addEventListener("click", velgAntrekk2);
 
 
+// function TogF() { //her inne skal vi si hva som skjer når noe er sant eller usant.
+    
+//     if (antrekk0TogF==false) { 
+//         antrekk0.style.display="none"
+//         } else {
+//             antrekk0.style.display="block" 
+//         }
+
+
+//     if (antrekk1TogF==false) { //om antrekket er av
+//         antrekk1.style.display="none"; //skal det ikke vises
+//         antrekk0TogF=true
+        
+//         } else { //men hvis det ikke er av (altså på)
+//             antrekk1.style.display="block"; //skal det vises
+//             antrekk0TogF=false
+            
+//         }
+
+//         if (antrekk2TogF==false) { 
+//             antrekk2.style.display="none"
+//             antrekk0TogF=true
+            
+//             } else {
+//                 antrekk2.style.display="block" 
+//                 antrekk0TogF=false
+                
+//             }
+
+// }
+
+
 function TogF() { //her inne skal vi si hva som skjer når noe er sant eller usant.
+
     if (antrekk1TogF==false) { //om antrekket er av
         antrekk1.style.display="none"; //skal det ikke vises
         } else { //men hvis det ikke er av (altså på)
             antrekk1.style.display="block"; //skal det vises
+            
         }
 
         if (antrekk2TogF==false) { 
@@ -29,37 +64,33 @@ function TogF() { //her inne skal vi si hva som skjer når noe er sant eller usa
                 antrekk2.style.display="block" 
             }
 
-            // if (antrekk0TogF==false) { 
-            //     antrekk0.style.display="none"
-            //     } else {
-            //         antrekk0.style.display="block" 
-            //     }
 }
 
-TogF() //function TogF blir kjørt her 
+TogF()//function TogF blir kjørt her 
 
 
 function velgAntrekk1() {
-console.log("Rosa ble trykket")
     //løp funksjonen så ting faktisk skjer.
     if (antrekk1TogF==false) {//3. Når trykket blir den til på
         antrekk1TogF=true
+        antrekk2TogF=false
         
-    } else {//4.når trykket igjen blir den på
+    } else {//4.når trykket igjen blir den til av
         antrekk1TogF=false
     }
-    TogF() //functionen blir kjørt
+TogF()
 }
 
 function velgAntrekk2() {
-    console.log("knapp2")
 
     if (antrekk2TogF==false) {
         antrekk2TogF=true
+        antrekk1TogF=false
     } else {
         antrekk2TogF=false
+        
     }
-    TogF()
+    TogF()//husk denne!!!!, ellers skjer det ingenting
 }
 
 
