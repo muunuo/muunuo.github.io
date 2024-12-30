3ef841d815a988cf5a592a5b57c5524c364873bb
muunuo <benkforland@gmail.com>
2024-12-30 16:04:41 +0100

    nettside farge
    Fant ut hvordan å sette det opp så hvis valg en og to er av så er defult på, og hvis valg en er på og valg to velges skal valg en av. Tok litt tid men ganske fornøyd

--- Javascript/JsJavascriptside/endreFarge.js
+++ Javascript/JsJavascriptside/endreFarge.js
@@ -4,11 +4,75 @@
 
 //Sette opp 4 farger man kan velge mellom
 
-//gjør fargene til en knapp
+
 //Når knapp trykkes endres fargen
 
 //Ekstra:
 // Gjør at det er en kombo text/farge endring basert på hva som er mer synelig, bruk et bilde av text på den fargen for å vise hvordan det vil se ut.
 //Når du hovrer vises det, npår du trykkes aktiveres det
 
+
+let normal = document.getElementById("farge1") //hva siden til vanelig ser ut som
+let høyKontrast = document.getElementById("farge2") //side som er optimal for de med dårlig syn
+
+let kontrast = false
+let vanelig = false
+
+høyKontrast.addEventListener("click", velgHøyKontrast);//gjør fargene til en knapp
+normal.addEventListener("click", velgNormal);
+
+function farge() {
+    if (kontrast==false) {
+        // document.body.style.backgroundColor = "#FFFFFF"
+        // document.body.style.color = "#000000"
+        // document.body.style.fontWeight = "normal"
+        defult()
+    } else {
+    console.log("høy kontrast er valgt")
+    document.body.style.backgroundColor = "#F7EDE2"
+    document.body.style.color = "#f5cac3"
+    document.body.style.fontWeight = "bold"
+    }
+    if (vanelig==false) {
+        // document.body.style.backgroundColor = "#FFFFFF"
+        // document.body.style.color = "#000000"
+        // document.body.style.fontWeight = "normal"
+        defult()
+    } else {
+        console.log("normal er valgt")
+        document.body.style.backgroundColor = "#f5cac3"
+        document.body.style.color = "#F7EDE2"
+    }
+}
+
+function defult() {
+    if (vanelig==false,kontrast==false) {
+        document.body.style.backgroundColor = "#FFFFFF"
+        document.body.style.color = "#000000"
+        document.body.style.fontWeight = "normal"
+    }
+}
+//if farge 1 og 2 er av skal defult være på. 
+
+function velgNormal() {
+    if (vanelig==false) {
+        vanelig=true
+        kontrast=false
+    } else {
+        vanelig=false
+    }
+    farge()
+}
+
+function velgHøyKontrast() {
+    if (kontrast==false) {
+        kontrast=true
+        vanelig=false
+    } else {
+        kontrast=false
+    }
+    farge()
+}
+
+//svaralternativ på quiz
 
