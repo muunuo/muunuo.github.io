c24737641a857812ac87490cfcb96561756090a3
muunuo <benkforland@gmail.com>
2025-01-01 14:55:58 +0100

    svar alternativ
    lå til et nytt spørsmål i quizen, denne gangen med to knapper som har svar alternativ og dom bruker kan velge mellom

--- Javascript/JsJavascriptside/spørsmål.js
+++ Javascript/JsJavascriptside/spørsmål.js
@@ -17,6 +17,60 @@ svar2 ="px";
 //lage svar knapp
 sendInn.addEventListener("click", sendInnSvar); //"click" er innebyggd,
 
+
+
+document.getElementById("poengsum").innerHTML =poengsum; //sier til bruker hva poeng de har.
+
+    
+
+let sa1 = document.getElementById("svarAlternativ1")
+let sa2 = document.getElementById("svarAlternativ2")
+
+sa1.addEventListener("click", Svar1); // lag to knapper
+sa2.addEventListener("click", Svar2); 
+
+valg1=false //når et valg er velgt blir det true
+valg2=false
+trykk()
+function trykk() {
+    if (valg1==false) {
+        svarAlternativ1.style.backgroundColor = "#b469b3"
+    } else {
+        svarAlternativ1.style.backgroundColor = "#955894" //når knapp trykkes bli værende trykket
+    }
+
+    if (valg2==false) {
+        svarAlternativ2.style.backgroundColor = "#b469b3"
+    } else {
+        svarAlternativ2.style.backgroundColor = "#955894"
+    }
+}
+
+function Svar1() {
+
+    if (valg1==false) {
+        valg1=true
+        valg2=false
+    } else {
+        valg1=false
+    }
+    
+    trykk()
+
+}
+
+function Svar2() {
+    if (valg2==false) {
+        valg2=true
+        valg1=false //hvis knapp 2 trykkes når knapp 1 er trykket resettes knapp1 og knapp 2 blir værende trykket
+    } else {
+        valg2=false
+    }
+    
+    trykk()
+}
+
+
 //få svar knappen til å sende inn svar for begge spørsmålene
 function sendInnSvar (){ //når send in brukernavn blir aktivert skal dette skje
     let svarSpørsmål1 = brukerSittSvarSpørsmål1.value; //lagrer brukernavnet som ble skrvet inn i feltet
@@ -30,6 +84,7 @@ function sendInnSvar (){ //når send in brukernavn blir aktivert skal dette skje
     //svaret kan ikke redigeres 
     document.getElementById("brukerSittSvarSpørsmål1").disabled= true;
     document.getElementById("brukerSittSvarSpørsmål2").disabled= true;
+
 
 
 //la bruker vite hva rett svar er 
@@ -49,17 +104,28 @@ function sendInnSvar (){ //når send in brukernavn blir aktivert skal dette skje
         SierRettSvar2.style.display="block";
     }
 
+    if (valg1==true) {
+        poengsum=poengsum+1//ene er rett å trykke, andre feil
+    } else if(valg2==true) {
+        poengsum=poengsum+0//når svar leveres gi poeng hvis riktig knapp er trykket 
+    }
+
     console.log(poengsum)
     document.getElementById("poengsum").innerHTML =poengsum; //sier til bruker hva poeng de har.
+
+    brukerSittSvarSpørsmål2.style.float="left"//riktig svar ligger ved sidnavs brukers svar
+    brukerSittSvarSpørsmål1.style.float="left"
+
+
 }
 
 
 
 
 
-document.getElementById("poengsum").innerHTML =poengsum; //sier til bruker hva poeng de har.
 
-    
+
+
 
 // //setter opp så input kan fjernes og svar vises 
 
